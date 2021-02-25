95783afab49109f660c106eba9636490ee332343
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-02-25 13:23:33 +0100

    start-snack-1

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
+    // PHP Snack 1:
+    // Creiamo un array 'matches',
+    // contenente altri array i quali rappresentano delle partite di basket di un’ipotetica tappa del calendario.
+    // Ogni array della partita avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+    // Stampiamo a schermo tutte le partite con questo schema:
+    // Olimpia Milano - Cantù | 55 - 60
+
+
+    
 
 ?>
 
@@ -13,7 +22,7 @@
 </head>
 <body>
     
-    
+
 
 </body>
 </html>
