9459976a1e02f4d0d5728428f71cc50b26433589
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-02-25 13:50:18 +0100

    start-snack-2

--- index.php
+++ index.php
@@ -56,6 +56,16 @@
     // Calcolo la lunghezza dell'array
     $matchesLength = count($matches);
 
+
+    // PHP Snack 2:
+    // Passare come parametri GET name, mail e age e verificare
+    // (cercando i metodi che non conosciamo nella documentazione) che:
+    // 1. name sia più lungo di 3 caratteri,
+    // 2. mail contenga un punto e una chiocciola
+    // 3. age sia un numero.
+    // Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”.
+
+
 ?>
 
 <!DOCTYPE html>
@@ -87,6 +97,9 @@
             </h3>
 
     <?php } ?>
+
+    <h2>Snack - 2</h2>
+
 
 
 </body>
